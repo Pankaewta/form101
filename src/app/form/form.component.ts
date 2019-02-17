d4f8d1cf1777c9b407bb9f5d8cf430a1de6e5791
panka <m.0983047804@gmail.com>
2019-02-17 11:25:17 +0700

    form102

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+  formGroup: FormGroup;
+  constructor(
+    private formBuild: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.formGroup = this.formBuild.group({
+      firstName: [''],
+      lastName: ['']
+    })
+  }
+
+}
